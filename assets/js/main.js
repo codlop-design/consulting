38674c85dbc07c337d773ca0d86feeb2809dd270
abdohantash <abdohantash9999@gmail.com>
2023-03-14 15:16:09 +0200

    responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,3 @@
-
-
-
 jQuery(function ($) {
 
     'use strict';
@@ -22,20 +19,20 @@ jQuery(function ($) {
         navResponsive();
     })
 
-    $('.menu .dropdown-menu').each(function() {
+    $('.menu .dropdown-menu').each(function () {
         var children = $(this).children('.dropdown').length;
-        $(this).addClass('children-'+children);
+        $(this).addClass('children-' + children);
     })
 
-    
-    $('.menu .nav-item.dropdown').each(function() {
+
+    $('.menu .nav-item.dropdown').each(function () {
         var children = $(this).children('.nav-link');
         children.addClass('prevent');
     })
 
     $(document).on('click', '#menu .nav-item .nav-link', function (e) {
 
-        if($(this).hasClass('prevent')) {
+        if ($(this).hasClass('prevent')) {
             e.preventDefault();
         }
 
@@ -43,7 +40,7 @@ jQuery(function ($) {
 
         nav_link.next().toggleClass('show');
 
-        if(nav_link.hasClass('smooth-anchor')) {
+        if (nav_link.hasClass('smooth-anchor')) {
             $('#menu').modal('hide');
         }
     })
@@ -56,10 +53,10 @@ jQuery(function ($) {
     'use strict';
 
     var position = $(window).scrollTop();
-    var navbar   = $('.navbar.sub');
-    var toTop    = $('#scroll-to-top');
+    var navbar = $('.navbar.sub');
+    var toTop = $('#scroll-to-top');
 
-    $(document).ready(function() {
+    $(document).ready(function () {
         if (position > 0) {
             navbar.addClass('navbar-sticky');
         }
@@ -80,7 +77,7 @@ jQuery(function ($) {
 
                 navbar.addClass('navbar-sticky');
 
-                if(navbar.hasClass('navbar-fixed') || window.innerWidth <= 767) {
+                if (navbar.hasClass('navbar-fixed') || window.innerWidth <= 767) {
                     navbar.removeClass('hidden').addClass('visible');
 
                 } else {
@@ -88,11 +85,11 @@ jQuery(function ($) {
                     if ($(window).scrollTop() >= window.innerHeight) {
                         navbar.removeClass('visible').addClass('hidden');
                     }
-                }                
+                }
 
                 toTop.fadeOut('fast');
 
-            // Up
+                // Up
             } else {
 
                 navbar.removeClass('hidden').addClass('visible');
@@ -101,9 +98,9 @@ jQuery(function ($) {
                 if ($(window).scrollTop() <= 100) {
                     navbar.removeClass('navbar-sticky');
 
-                } else {                   
+                } else {
 
-                    if(!navbar.hasClass('navbar-no-fixed')) {
+                    if (!navbar.hasClass('navbar-no-fixed')) {
                         navbar.addClass('visible');
                     }
                 }
@@ -134,7 +131,7 @@ jQuery(function ($) {
     let navbar_holder = $('.navbar-holder');
     let navbar_height = $('.navbar-expand.sub').outerHeight();
 
-    if(navbar_holder.length > 0) {
+    if (navbar_holder.length > 0) {
         $('.navbar-holder').css('min-height', navbar_height);
     }
 })
@@ -144,8 +141,8 @@ var swiper = new Swiper(".clients-slide", {
     loop: true,
     speed: 5000,
     autoplay: {
-      enabled: true,
-      delay: 1,
+        enabled: true,
+        delay: 1,
     }
 });
 
@@ -154,4 +151,3 @@ $(".clients-slide").hover(function () {
 }, function () {
     (this).swiper.autoplay.start();
 });
-
